43e60220c2601acc027a0aa7dd40996754940c74
klrqn <burtonsb@gmail.com>
2018-06-19 15:07:16 +0000

    add comments

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> // stdio.h contains the standard input/output library, which means that it deals with input (like from the keyboard) and output (printing characters to the screen). It contains the code of printf that we are using.
 
+// main is the standard name in C to indicate that it is the default function in a program that should be run
 int main(void)
 {
     printf("hello, world\n");
